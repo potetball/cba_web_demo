0d5b7adfea31ab8034ad53b353ef3aedab569715
potetball <tyleri17@student.westerdals.no>
2018-10-20 00:33:13 +0200

    added shared library, database and other stuff.

---
+++ CBASharedLibrary/Models/CBAUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CBASharedLibrary
+{
+  public class CBAUser
+  {
+    public DateTime? LastActive { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Fullnane { get; set; }
+
+    public string Id { get; set; }
+  }
+
+}
